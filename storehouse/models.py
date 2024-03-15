5f3369a28ffd08a6684138fbe72c40ec2f2344d1
giovanni <giovidoh@gmail.com>
2024-03-15 21:07:51 +0000

    refactor: removing rest_framework imports from storehouse.models

--- storehouse/models.py
+++ storehouse/models.py
@@ -5,8 +5,6 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from rest_framework import status
-from rest_framework.response import Response
 
 # Create your models here.
 class Storehouse(models.Model):
