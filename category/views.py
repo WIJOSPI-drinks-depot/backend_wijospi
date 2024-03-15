4799385e954d3ae04c737c58524451838a98865b
giovanni <giovidoh@gmail.com>
2024-03-15 21:06:02 +0000

    feat: Create function added to category.views & corsheaders added to settings.py

--- category/views.py
+++ category/views.py
@@ -14,6 +14,27 @@ class CategoryViewset(ModelViewSet):
         
         return queryset
     
+    def create(self, request, *args, **kwargs):
+        # Vérifier si le contenu de la requête est JSON
+        if request.content_type == 'application/json':
+            # Analyser les données JSON envoyées dans la requête
+            json_data = request.data
+            
+            # Extraire les données de l'objet JSON
+            category_name = json_data.get('category_name')
+            
+            # Créer une nouvelle instance de catégorie
+            category = Category.objects.create(name=category_name)
+            
+            # Serializer la nouvelle instance de catégorie
+            serializer = CategorySerializer(category)
+            
+            # Retourner une réponse avec les données sérialisées
+            return Response(serializer.data, status=201)
+        else:
+            # Si le contenu de la requête n'est pas JSON, renvoyer une erreur
+            return Response({'error': 'Invalid content type'}, status=400)
+    
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.soft_delete()
