1c245e5db3bd2b3b0758dddd7cb85a2da05de969
giovanni <giovidoh@gmail.com>
2024-04-13 00:15:12 +0000

    perf: supply update

--- backend_wijospi/urls.py
+++ backend_wijospi/urls.py
@@ -6,7 +6,7 @@ from rest_framework import routers
 from category.views import CategoryViewset
 from packaging.views import PackagingViewset
 from storehouse.views import StorehouseViewset
-from storehouse.views import StorehouseDrinkRackViewset
+# from storehouse.views import StorehouseDrinkRackViewset # Si on veut ajouter plusieurs boissons à un même approvisionnement
 from customer.views import CustomerViewset
 from drink_rack.views import DrinkRackViewset
 from supply.views import SupplyViewset
@@ -18,7 +18,7 @@ router = routers.SimpleRouter()
 router.register('category', CategoryViewset, basename='category')
 router.register('packaging', PackagingViewset, basename='packaging')
 router.register('storehouse', StorehouseViewset, basename='storehouse')
-router.register('stock', StorehouseDrinkRackViewset, basename='stock')
+# router.register('stock', StorehouseDrinkRackViewset, basename='stock') # Si on veut ajouter plusieurs boissons à un même approvisionnement
 router.register('customer', CustomerViewset, basename='customer')
 router.register('drink-rack', DrinkRackViewset, basename='drink-rack')
 router.register('supply', SupplyViewset, basename='supply')
--- supply/models.py
+++ supply/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Supply(models.Model):
     date_time = models.DateTimeField(default=datetime.now)
     storehouse = models.ForeignKey('storehouse.Storehouse', on_delete=models.CASCADE)
-    drink_racks = models.ManyToManyField('drink_rack.DrinkRack', through='SupplyDrinkRack')
+    drink_rack = models.ForeignKey('drink_rack.DrinkRack', on_delete=models.CASCADE)
+    quantity = models.IntegerField() # Quantité de casiers à approvisionner
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, default=None)
@@ -16,16 +17,4 @@ class Supply(models.Model):
     def soft_delete(self):
         self.deleted_at = datetime.now()
         self.save()
-
-# Relation n à n entre APPROVISIONNEMENT et CASIER_BOISSON
-class SupplyDrinkRack(models.Model):
-    supply = models.ForeignKey(Supply, verbose_name=("Approvisionnement"), on_delete=models.CASCADE)
-    drink_rack = models.ForeignKey("drink_rack.DrinkRack", verbose_name=("Casier de boisson"), on_delete=models.CASCADE)
-    supply_quantity = models.IntegerField() # La quantité approvisionnée du casier de boisson
-    
-    class Meta:
-        unique_together = ('supply', 'drink_rack')
-    
-    def __str__(self):
-        return f"{self.supply} - {self.drink_rack}"
     
